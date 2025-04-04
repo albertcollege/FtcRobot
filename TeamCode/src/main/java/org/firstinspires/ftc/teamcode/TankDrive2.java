22fadca867e5d147bba024b965495eb530f9eac7
Yifan Jin <JYF2868@outlook.com>
2025-04-04 12:56:52 -0400

    Update TankDrive2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive2.java
@@ -21,24 +21,25 @@ public class TankDrive2 extends OpMode {
     @Override
     public void loop() {
         // --- Drive Motors ---
-        double power = -gamepad1.left_stick_y;
-        double  turn = -gamepad1.left_stick_x;
+        double drive = -gamepad1.left_stick_y;
+        double turn = -gamepad1.left_stick_x;
 
+        double leftPower = drive + turn;
+        double rightPower = drive - turn;
 
-        leftMotor.setPower(power);
-        rightMotor.setPower(power);
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
 
-        leftMotor.setPower(-turn);
-        rightMotor.setPower(turn);
+        double max = Math.max (Math. abs(leftPower), Math.abs(rightPower));
 
 
         // --- Top Motor Control ---
         double topPower = 0.1; // Default power is 0.1
 
         if (gamepad1.dpad_up) {
-            topPower = 0.6;
+            topPower = 0.4;
         } else if (gamepad1.dpad_down) {
-            topPower = -0.6;
+            topPower = -0.4;
         }
         // If neither is pressed, topPower remains 0.0 as set initially
 
@@ -50,7 +51,7 @@ public class TankDrive2 extends OpMode {
         telemetry.addData("Right Stick Y", gamepad1.right_stick_y);
         telemetry.addData("Dpad Up", gamepad1.dpad_up);
         telemetry.addData("Dpad Down", gamepad1.dpad_down);
-        telemetry.addData("Power", power);
+        telemetry.addData("Power", drive);
         telemetry.addData("Top Power", topPower);
         telemetry.update();
     }
